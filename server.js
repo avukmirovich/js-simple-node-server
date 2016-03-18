f545277d750a3f99d85ae15842e1b0d488ef7fcc
Alexander Vukmirovich <alex.vukmirovich@gmail.com>
2016-03-18 10:16:37 +0800

    Add requests logging

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var fs = require('fs');
 var serverRoot = path.join(__dirname, 'public');
 var serverIndex = 'index.html';
 
+function logRequest(method, fileName, statusCode) {
+    console.log([method, fileName, statusCode].join(' -> '));
+}
+
 http.createServer(function(req, res) {
 
     if (req.method != 'GET') {
@@ -14,11 +18,16 @@ http.createServer(function(req, res) {
         return;
     }
 
-    var filePath = path.join(serverRoot, url.parse(req.url).pathname);
+    var fileName = url.parse(req.url).pathname;
+    var filePath = path.join(serverRoot, fileName);
+    var method = req.method;
+    var statusCode = 404;
+
     fs.stat(filePath, function(err, stats) {
         if (err) {
             res.writeHead(404, 'Not Found');
             res.end();
+            logRequest(method, fileName, statusCode);
         } else {
             if (stats.isDirectory()) {
                 filePath = path.join(filePath, serverIndex);
@@ -28,12 +37,16 @@ http.createServer(function(req, res) {
                 if (err) {
                     res.writeHead(404, 'Not Found');
                     res.end();
+                    logRequest(method, fileName, statusCode);
                 } else {
+                    statusCode = 200;
                     res.writeHead(200, 'OK');
                     fs.createReadStream(filePath).pipe(res);
+                    logRequest(method, fileName, statusCode);
                 }
             });
         }
+
     });
 
 }).listen(process.env.PORT || 3000);
